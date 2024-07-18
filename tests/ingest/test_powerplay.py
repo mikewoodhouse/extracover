089cf41eacd5d83f0d802b335e4653aaf02a1c5b
Mike Woodhouse <mikewoodhouse@gmail.com>
2024-07-18 14:30:04 +0100

    tidy-up

--- tests/ingest/test_powerplay.py
+++ tests/ingest/test_powerplay.py
@@ -5,4 +5,4 @@ def test_powerplay_properties():
     pp = PowerPlay(type="", ball_from=0.1, ball_to=5.6)
     assert pp.first_over == 0
     assert pp.last_over == 5
-    assert pp.overs == range(0, 6)
+    assert pp.overs == range(6)
