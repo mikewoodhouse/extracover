ce18ff32f9ba56f7f33e09868af6e4efcd9472b7
Mike Woodhouse <mikewoodhouse@gmail.com>
2024-11-01 16:00:45 +0000

    feat: start ball evaluation

--- app/model/balluator.py
+++ app/model/balluator.py
@@ -1,16 +1,26 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from random import Random
+
+from .scorebook import Ball
 
 
 @dataclass
 class BallParams:
-    p_wicket: float
-    p_wide: float
-    p_wide_runs: list[float]
-    p_noball: float
-    p_nb_runs: list[float]
-    p_bye_runs: list[float]
-    p_runs: list[float]
+    p_wicket: float = 0.0
+    p_wide: float = 0.0
+    p_wide_runs: list[float] = field(default_factory=list)
+    p_noball: float = 0.0
+    p_nb_runs: list[float] = field(default_factory=list)
+    p_bye_runs: list[float] = field(default_factory=list)
+    p_runs: list[float] = field(default_factory=list)
 
 
 class Balluator:
-    pass
+    def __init__(self, rnd: Random | None) -> None:
+        self.rnd = rnd or Random()
+
+    def result(self, params: BallParams) -> Ball:
+        ball = Ball()
+        if self.rnd.random() <= params.p_wicket:
+            ball.wicket_fell = True
+        return ball
--- tests/model/test_balluator.py
+++ tests/model/test_balluator.py
@@ -1,5 +1,44 @@
-from app.model.balluator import Balluator
+from dataclasses import dataclass, field
+from random import Random
+
+import pytest
+
+from app.model.balluator import BallParams, Balluator
+from app.model.scorebook import Ball
+
+
+@dataclass
+class NotRandom(Random):
+    sequence: list[float] = field(default_factory=list)
+    idx: int = 0
+
+    def random(self) -> float:
+        next_val = self.sequence[self.idx]
+        self.idx += 1
+        return next_val
 
 
 def test_creation():
-    assert Balluator()
+    rng = NotRandom()
+    assert Balluator(rng)
+
+
+@pytest.mark.parametrize(
+    "parms,expected,rands,desc",
+    [
+        (BallParams(), Ball(), [1.0], "nothing happens"),
+        (
+            BallParams(p_wicket=0.05),
+            Ball(wicket_fell=True),
+            [
+                0.01,
+            ],
+            "wicket",
+        ),
+    ],
+)
+def test_result(parms, expected, rands, desc):
+    rng = NotRandom()
+    rng.sequence = rands
+    obj = Balluator(rng)
+    assert obj.result(parms) == expected, desc
