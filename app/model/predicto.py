86374edd2eec084d125092b7c866263ab3308ae9
Mike Woodhouse <mikewoodhouse@gmail.com>
2024-11-04 12:47:54 +0000

    feat: describe predicto (first attempt)

---
+++ app/model/predicto.py
@@ -0,0 +1,17 @@
+# TODO: first-cut prediction:
+# Pull base over rates - restrict to a date range? Needed for model tests later but not yet
+# Teams & toss - start with first innings only?
+# Pull per-bowler/per-batter over rates
+# bowling order
+# TODO: fall of wickets & batter propensities to get out?
+# TODO: over rates by wickets down?
+
+
+class Predicto:
+    """
+    Controls the simulation of a match from a supplied state (which may be at the start)
+    until its  conclusion, producing a MatchState object from which, with a suitably large
+    number of others, distributions of interesting outcomes may be derived.
+    """
+
+    pass
