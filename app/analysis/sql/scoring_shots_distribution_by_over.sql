ee8298ef48fa4da536da4fb58cec1f0d517c2cbe
Mike Woodhouse <mikewoodhouse@gmail.com>
2024-11-21 18:05:28 +0000

    feat: baseline scoring shot distribution

--- app/analysis/sql/scoring_shots_distribution_by_over.sql
+++ app/analysis/sql/scoring_shots_distribution_by_over.sql
@@ -4,26 +4,37 @@
 	This is the "baseline" for batters
 */
 WITH 
-	legit_balls AS (
-	SELECT
-	  over
-	, wickets_so_far
-	, batter_runs AS runs
-	FROM all_balls
-	WHERE LENGTH(extra_type) = 0
-	AND NOT wicket_fell
-)
+  included_matches AS (
+    SELECT
+      rowid
+    FROM
+      matches
+    WHERE
+      start_date <= :max_match_date
+  )
+, non_extra_balls AS (
+    SELECT
+      b.*
+    FROM
+      all_balls b
+      JOIN included_matches m ON m.rowid = b.match_id
+    WHERE
+      b.innings < 2
+      AND b.extra_type = ''
+      AND NOT b.wicket_fell
+  )
+, 
 SELECT
 	over
-,	wickets_so_far
-,	(SUM(CASE WHEN runs = 0 THEN 1 END) + 0.0) / SUM(Count(*)) OVER (PARTITION BY over, wickets_so_far) AS "0"
-,	(SUM(CASE WHEN runs = 1 THEN 1 END) + 0.0) / SUM(Count(*)) OVER (PARTITION BY over, wickets_so_far) AS "1"
-,	(SUM(CASE WHEN runs = 2 THEN 1 END) + 0.0) / SUM(Count(*)) OVER (PARTITION BY over, wickets_so_far) AS "2"
-,	(SUM(CASE WHEN runs = 3 THEN 1 END) + 0.0) / SUM(Count(*)) OVER (PARTITION BY over, wickets_so_far) AS "3"
-,	(SUM(CASE WHEN runs = 4 THEN 1 END) + 0.0) / SUM(Count(*)) OVER (PARTITION BY over, wickets_so_far) AS "4"
-,	(SUM(CASE WHEN runs = 5 THEN 1 END) + 0.0) / SUM(Count(*)) OVER (PARTITION BY over, wickets_so_far) AS "5"
-,	(SUM(CASE WHEN runs = 6 THEN 1 END) + 0.0) / SUM(Count(*)) OVER (PARTITION BY over, wickets_so_far) AS "6"
+,	wickets_down
+,	(SUM(CASE WHEN batter_runs = 0 THEN 1.0 END)) / SUM(Count(*)) OVER (PARTITION BY over, wickets_down) AS "0"
+,	(SUM(CASE WHEN batter_runs = 1 THEN 1.0 END)) / SUM(Count(*)) OVER (PARTITION BY over, wickets_down) AS "1"
+,	(SUM(CASE WHEN batter_runs = 2 THEN 1.0 END)) / SUM(Count(*)) OVER (PARTITION BY over, wickets_down) AS "2"
+,	(SUM(CASE WHEN batter_runs = 3 THEN 1.0 END)) / SUM(Count(*)) OVER (PARTITION BY over, wickets_down) AS "3"
+,	(SUM(CASE WHEN batter_runs = 4 THEN 1.0 END)) / SUM(Count(*)) OVER (PARTITION BY over, wickets_down) AS "4"
+,	(SUM(CASE WHEN batter_runs = 5 THEN 1.0 END)) / SUM(Count(*)) OVER (PARTITION BY over, wickets_down) AS "5"
+,	(SUM(CASE WHEN batter_runs = 6 THEN 1.0 END)) / SUM(Count(*)) OVER (PARTITION BY over, wickets_down) AS "6"
 FROM
-	legit_balls
-GROUP BY over, wickets_so_far
-ORDER BY over, wickets_so_far
+	non_extra_balls
+GROUP BY over, wickets_down
+ORDER BY over, wickets_down
