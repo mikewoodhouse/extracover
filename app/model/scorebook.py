9bbe0b52bc4ac0f855d8a69c0fba7b0bd31a72a1
Mike Woodhouse <mikewoodhouse@gmail.com>
2024-11-25 17:20:29 +0000

    refactor: move match -> scorebook

--- app/model/scorebook.py
+++ app/model/scorebook.py
@@ -1,6 +1,20 @@
 from __future__ import annotations
 
 from dataclasses import dataclass, field
+from datetime import date
+
+
+@dataclass
+class Match:
+    match_id: int
+    start_date: date
+    match_type: str  # may want to become an enum?
+    gender: str  # enum?
+    venue: str
+    event: str  # might want to become a foreign key to a future events table?
+    city: str
+    overs: int
+    balls_per_over: int
 
 
 @dataclass
