46a7daf71afe5a1dca78d6d7cf18660d995913b2
Mike Woodhouse <mikewoodhouse@gmail.com>
2024-12-02 16:08:27 +0000

    feat: include start_date for splitting train/test sets

--- app/model/dataset_builder.py
+++ app/model/dataset_builder.py
@@ -1,10 +1,19 @@
+from __future__ import annotations
+
+from collections import defaultdict
 from dataclasses import dataclass, field
 from datetime import date
 from pathlib import Path
 
+from dataclass_csv import DataclassWriter
 from dataclasses_json import Undefined, dataclass_json
 
 from app.utils import Database, StopWatch
+
+MIN_MATCHES = 1000
+AVG_RUNS_PER_BALL = 1.21
+AVG_WICKET_PROB = 0.054
+AVG_WIDE_NOBALL_RATE = 0.036
 
 
 def empty_list(size: int) -> list[int]:
@@ -14,6 +23,9 @@ def empty_list(size: int) -> list[int]:
 def sql_text(query_filename: str) -> str:
     path = Path(__file__).parent / f"{query_filename}.sql"
     return path.read_text()
+
+
+DATA_DIR = Path(__file__).parent.parent.parent / "data"
 
 
 @dataclass_json(undefined=Undefined.EXCLUDE)
@@ -40,6 +52,8 @@ class Ball:
     dismissed: str
     how_out: str
 
+    # region methods
+
     @property
     def wide_noball(self) -> bool:
         return self.extra_type in ("wide", "noball")
@@ -51,6 +65,8 @@ class Ball:
     @property
     def was_legal(self) -> bool:
         return self.extra_type == ""
+
+    # endregion
 
 
 @dataclass_json(undefined=Undefined.EXCLUDE)
@@ -69,6 +85,8 @@ class Player:
     wides: int = 0
     runs_conceded: int = 0
     wickets_taken: int = 0
+
+    # region methods
 
     def record_batting_position(self, pos: int) -> None:
         pos = min(
@@ -99,12 +117,8 @@ class Player:
         self.balls_faced += 1
         if ball.wicket_fell and ball.dismissed == self.name:
             self.times_out += 1
-        else:
-            if not ball.wide_noball:
-                runs = min(
-                    ball.batter_runs, 6
-                )  # count weird penalty additions as sixes
-                self.scoring_shots[runs] += 1
+        elif not ball.wide_noball:
+            self.scoring_shots[min(ball.batter_runs, 6)] += 1
 
     @property
     def total_runs_scored(self) -> int:
@@ -115,19 +129,34 @@ class Player:
 
     @property
     def strike_rate(self) -> float:
+        """
+        runs per ball
+        """
         return (
-            0.0
-            if self.balls_faced == 0
-            else self.total_runs_scored * 100.0 / self.balls_faced
+            AVG_RUNS_PER_BALL
+            if self.balls_faced < 10
+            else self.total_runs_scored / self.balls_faced
         )
 
     @property
     def economy(self) -> float:
+        """
+        runs per ball
+        """
         return (
-            0.0
-            if self.balls_bowled == 0
-            else 6.0 * self.runs_conceded / self.balls_bowled
+            AVG_RUNS_PER_BALL
+            if self.balls_bowled < 12
+            else self.runs_conceded / self.balls_bowled
         )
+
+    @property
+    def wicket_prob(self) -> float:
+        """ "
+        per ball
+        """
+        if self.balls_bowled < 24 or self.wickets_taken < 1:
+            return AVG_WICKET_PROB
+        return float(self.wickets_taken) / self.balls_bowled
 
     @property
     def wide_rate(self) -> float:
@@ -148,15 +177,22 @@ class Player:
         )
         return s.replace("self.", "")
 
+    # endregion
+
 
 @dataclass
 class MatchState:
+    start_date: date
+    innings: int = 0
     total: int = 0
     target: int = 0
     wickets: int = 0
     balls_bowled: int = 0
-    run_rate: float = 0.0
-    req_rate: float = 0.0
+    run_rate: float = 0.0  # per *ball*
+    req_rate: float = 0.0  # per *ball*
+    balls_faced: defaultdict[int, int] = field(default_factory=lambda: defaultdict(int))
+
+    # region methods
 
     def update(self, ball: Ball) -> None:
         self.total += ball.batter_runs + ball.extra_runs
@@ -164,47 +200,201 @@ class MatchState:
             self.wickets += 1
         if ball.was_legal:
             self.balls_bowled += 1
-        self.run_rate = 6.0 * self.total / self.balls_bowled if self.balls_bowled else 0
+        self.run_rate = (
+            float(self.total) / self.balls_bowled if self.balls_bowled else 0.0
+        )
+        self.balls_faced[ball.batter] += 1
         if ball.innings == 1:
             self.req_rate = (
-                (self.target + 1.0) / (120.0 - self.balls_bowled)
+                (self.target - self.total + 1.0) / (120.0 - self.balls_bowled)
                 if self.balls_bowled < 120
-                else 0
+                else 0.0
             )
+            # print(
+            #     f"{self.target=} {self.total=}/{self.wickets} {self.balls_bowled} {self.req_rate=}"
+            # )
+
+    # endregion
+
+
+@dataclass
+class MLRow:
+    # inputs
+    start_date: date
+    innings: int
+    ball_of_innings: int
+    # in_powerplay: int
+    # in_last_two: int
+    wickets_down: int
+    run_rate: float
+    req_rate: float  # what value when first innings?
+    batter_in_first_10: int
+    batter_strike_rate: float
+    bowler_economy: float
+    bowler_wicket_prob: float
+    bowler_wide_noball_rate: float
+    # outputs - what actually happened
+    outcome: int
+    # wide: int
+    # noball: int
+    # bye: int
+    # legbye: int
+    # wicket: int
+    # dot_ball: int
+    # single: int
+    # two: int
+    # three: int
+    # four: int
+    # six: int
+
+    @classmethod
+    def build(
+        cls, ball: Ball, state: MatchState, batter: Player, bowler: Player
+    ) -> MLRow:
+        # wide = int(ball.extra_type == "wide"),
+        # noball = int(ball.extra_type == "noball"),
+        # bye = int(ball.extra_type == "bye"),
+        # legbye = int(ball.extra_type == "legbye"),
+        # wicket = int(ball.wicket_fell),
+        # dot_ball = int(ball.batter_runs == 0),
+        # single = int(ball.batter_runs == 1),
+        # two = int(ball.batter_runs == 2),
+        # three = int(ball.batter_runs == 3),
+        # four = int(ball.batter_runs == 4),
+        # six = int(ball.batter_runs > 4),
+        outcome = -1
+        if ball.extra_type == "wide":
+            outcome = 0
+        elif ball.extra_type == "noball":
+            outcome = 1
+        elif ball.extra_type == "bye":
+            outcome = 2
+        elif ball.extra_type == "legbye":
+            outcome = 3
+        elif ball.wicket_fell:
+            outcome = 4
+        elif ball.batter_runs == 0:
+            outcome = 5
+        elif ball.batter_runs == 1:
+            outcome = 6
+        elif ball.batter_runs == 2:
+            outcome = 7
+        elif ball.batter_runs == 3:
+            outcome = 8
+        elif ball.batter_runs == 4:
+            outcome = 9
+        elif ball.batter_runs > 4:
+            outcome = 10
+        else:
+            print(ball)
+            outcome = -1
+        return MLRow(
+            start_date=state.start_date,
+            innings=state.innings,
+            ball_of_innings=state.balls_bowled,
+            # in_powerplay=int(ball.over < 6),
+            # in_last_two=int(ball.over > 17),
+            wickets_down=state.wickets,
+            run_rate=state.run_rate,
+            req_rate=state.req_rate,
+            batter_in_first_10=int(state.balls_faced[batter.player_id] <= 10),
+            batter_strike_rate=batter.strike_rate,
+            bowler_economy=bowler.economy,
+            bowler_wicket_prob=bowler.wicket_prob,
+            bowler_wide_noball_rate=(
+                AVG_WIDE_NOBALL_RATE
+                if bowler.balls_bowled < 24
+                else bowler.wide_rate + bowler.noball_rate
+            ),
+            outcome=outcome,
+        )
+
+    @staticmethod
+    def in_bounds(row: MLRow) -> bool:
+        return row.req_rate <= 6.0
 
 
 class DatasetBuilder:
-    def __init__(self, db: Database) -> None:
+    def __init__(self, db: Database, warm_up_match_count: int = 1000) -> None:
         self.db = db
         self.players: dict[int, Player] = {}
+        self.ml_rows_powerplay: list[MLRow] = []
+        self.ml_rows_middle: list[MLRow] = []
+        self.ml_rows_last_two: list[MLRow] = []
+        self.warm_up_match_count = warm_up_match_count
+        self.matches_run = 0
 
     def run(self) -> None:
-        with StopWatch(msg="match loop", decimals=5) as stopwatch:
-            for row in self.db.query_result(
-                "SELECT ROWID AS match_id, * FROM matches ORDER BY start_date"
-            ):
-                match = Match(**row)
-                self.process_match(match.match_id)
-                stopwatch.tick()
+        with StopWatch(msg="DatasetBuilder.run()"):
+            with StopWatch(msg="match loop", decimals=5) as stopwatch:
+                for row in self.db.query_result(
+                    "SELECT ROWID AS match_id, * FROM matches ORDER BY start_date"
+                ):
+                    match = Match(**row)
+                    self.process_match(match)
+                    self.matches_run += 1
+                    stopwatch.tick()
 
-    def process_match(self, match_id: int):
+            csv_path = DATA_DIR / "ml_rows_powerplay.csv"
+            with csv_path.open("w") as csv:
+                dataclass_writer = DataclassWriter(csv, self.ml_rows_powerplay, MLRow)
+                print(f"writing {len(self.ml_rows_powerplay)} powerplay ML rows")
+                dataclass_writer.write()
+
+            csv_path = DATA_DIR / "ml_rows_middle.csv"
+            with csv_path.open("w") as csv:
+                dataclass_writer = DataclassWriter(csv, self.ml_rows_middle, MLRow)
+                print(f"writing {len(self.ml_rows_middle)} middle-game ML rows")
+                dataclass_writer.write()
+
+            csv_path = DATA_DIR / "ml_rows_last_two.csv"
+            with csv_path.open("w") as csv:
+                dataclass_writer = DataclassWriter(csv, self.ml_rows_last_two, MLRow)
+                print(f"writing {len(self.ml_rows_last_two)} ML last 2 overs rows")
+                dataclass_writer.write()
+
+    def process_match(self, match: Match) -> None:
+        match_id, start_date = match.match_id, match.start_date
         self.add_players(match_id)
         target = 0
         for innings in range(2):
             has_batted: set[int] = set()
-            state = MatchState(target=target if innings == 1 else 0)
+            state = MatchState(
+                start_date=start_date,
+                innings=innings,
+                target=target if innings == 1 else 0,
+            )
             for row in self.db.query_result(
                 """SELECT
-                        * FROM balls
-                        WHERE match_id = :match_id
-                        AND innings = :innings
-                        ORDER BY over, ball_seq
-                        """,
-                {"match_id": match_id, "innings": innings},
+                        *
+                    FROM balls
+                    WHERE match_id = :match_id
+                    AND innings = :innings
+                    ORDER BY over, ball_seq
+                    """,
+                {
+                    "match_id": match_id,
+                    "innings": innings,
+                },
             ):
                 ball = Ball(**row)
-                self.update_for_ball(has_batted, ball)
+
+                batter = self.players[ball.batter]
+                bowler = self.players[ball.bowled_by]
+
+                ml_row = MLRow.build(ball, state, batter, bowler)
+
+                if self.matches_run > MIN_MATCHES and MLRow.in_bounds(ml_row):
+                    if ball.over < 6:
+                        self.ml_rows_powerplay.append(ml_row)
+                    elif ball.over < 17:
+                        self.ml_rows_middle.append(ml_row)
+                    else:
+                        self.ml_rows_last_two.append(ml_row)
+
+                self.update_for_ball(has_batted, ball, batter, bowler)
                 state.update(ball)
+            # print(state)
             target = state.total
 
     def add_players(self, match_id):
@@ -222,10 +412,7 @@ class DatasetBuilder:
                 self.players[pid] = Player(**row)
             self.players[pid].matches += 1
 
-    def update_for_ball(self, has_batted, ball):
-        batter = self.players[ball.batter]
-        bowler = self.players[ball.bowled_by]
-
+    def update_for_ball(self, has_batted, ball, batter, bowler):
         # batting order
         if ball.batter not in has_batted:
             batter.record_batting_position(len(has_batted))
