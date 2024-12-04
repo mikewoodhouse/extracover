3b2525bff1563bab66c26f76da181f3f6c6cf951
Mike Woodhouse <mikewoodhouse@gmail.com>
2024-12-04 19:49:26 +0000

    feat: extending tests (slightly)

--- tests/model/test_predicto.py
+++ tests/model/test_predicto.py
@@ -1,6 +1,47 @@
+import pytest
+
 from app.model.predicto import Predicto
-from app.model.scorebook import Scorebook
+from app.model.scorebook import InningsCard, Match, Scorebook
+
+"""
+# Setting up the model
+
+For one simulation of a match we need to set up the Scorebook with
+* Player info
+    * historic stats as per the ML model
+* Teams
+    * 2 x Innings, with batting & bowling order
+
+We also need:
+* ML model
+
+For multiple sims, we additionally need
+* Extended Player info
+    * historic batting position record for batting order generation
+    * historic overs bowled record for bowling order generation
+"""
 
 
-def test_it_builds():
-    assert Predicto(Scorebook()) is not None
+def fake_match() -> Match:
+    return Match()
+
+
+def fake_first_innings() -> InningsCard:
+    return InningsCard()
+
+
+def fake_second_innings() -> InningsCard:
+    return InningsCard(batters=[])
+
+
+@pytest.fixture
+def fake_scorebook() -> Scorebook:
+    return Scorebook(
+        match=fake_match(),
+        first_innings=fake_first_innings(),
+        second_innings=fake_second_innings(),
+    )
+
+
+def test_it_builds(fake_scorebook):
+    assert Predicto(fake_scorebook) is not None
