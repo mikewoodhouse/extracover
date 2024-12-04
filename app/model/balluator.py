9384ae2936edf354a5a6914c6d9ff8db59ec1992
Mike Woodhouse <mikewoodhouse@gmail.com>
2024-12-04 10:45:57 +0000

    refactor: ball evaluation following kmeans probability exploration

--- app/model/balluator.py
+++ app/model/balluator.py
@@ -1,22 +1,80 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass
+from enum import IntEnum
 from random import Random
 
 from .scorebook import Ball
 
+# hard-code these for now - any situation-dependant variations are probably 2nd- or 3rd-order
+# weights derived from all male T20 matches up to & including 2024-04-07
+
+# try to deal with (not well identified) competitions where a no-ball costs 2: can spot them when
+# there are batter runs but not entirely where the penalty was 1 and batters ran a bye. Anyway, close enough...
+# with noballs AS (
+# 	select CASE batter_runs WHEN 0 THEN extra_runs ELSE 1 END AS extras
+# 	FROM balls WHERE extra_type = 'noball')
+# select extras, count(*) from noballs group by extras order by extras
+NB_EXTRA_RUNS: list[int] = [0, 6293, 972, 66, 87, 0, 38]
+
+# select count(*) from balls where extra_type = 'noball' group by batter_runs order by batter_runs
+NB_BATTER_RUNS: list[int] = [3500, 2039, 449, 19, 928, 0, 521]
+
+# select count(*) from balls where extra_type = 'wide' group by batter_runs order by batter_runs
+WIDE_EXTRA_RUNS: list[int] = [0, 50490, 2373, 505, 84, 1942]
+
+
+BYE_EXTRA_RUNS: list[int] = [0, 3722, 463, 61, 1438, 4]
+
+
+LEGBYE_EXTRA_RUNS: list[int] = [0, 23300, 1423, 128, 1901, 31, 1]
+
+
+class Outcome(IntEnum):
+    WIDE = 0
+    NOBALL = 1
+    BYE = 2
+    LEGBYE = 3
+    WICKET = 4
+    DOTBALL = 5
+    SINGLE = 6
+    TWO = 7
+    THREE = 8
+    FOUR = 9
+    SIX = 10
+
 
 @dataclass
 class BallParams:
-    p_noball: float = 0.0
     p_wide: float = 0.0
+    p_noball: float = 0.0
+    p_bye: float = 0.0
+    p_legbye: float = 0.0
     p_wicket: float = 0.0
-    p_nb_runs: list[float] = field(default_factory=list)
-    p_wide_runs: list[float] = field(default_factory=list)
-    p_runs: list[float] = field(default_factory=list)
-    p_bye_runs: list[float] = field(default_factory=list)
+    p_dotball: float = 0.0
+    p_single: float = 0.0
+    p_two: float = 0.0
+    p_three: float = 0.0
+    p_four: float = 0.0
+    p_six: float = 0.0
 
     @property
     def p_wnb(self) -> float:
         return self.p_wide + self.p_noball
+
+    @property
+    def probability_array(self) -> list[float]:
+        return [
+            self.p_wide,
+            self.p_noball,
+            self.p_bye,
+            self.p_legbye,
+            self.p_wicket,
+            self.p_dotball,
+            self.p_single,
+            self.p_two,
+            self.p_three,
+            self.p_four,
+            self.p_six,
+        ]
 
 
 class Balluator:
@@ -24,8 +82,22 @@ class Balluator:
         self.rnd = rnd or Random()
 
     def result(self, params: BallParams) -> Ball:
-        r_wnb = self.rnd.random()
-        return self.resolve_wnb(params, r_wnb) if r_wnb <= params.p_wnb else Ball()
-
-    def resolve_wnb(self, params: BallParams, r_wnb: float) -> Ball:
-        return Ball(noball=True) if r_wnb < params.p_noball else Ball(wide=True)
+        outcome = self.rnd.choices(
+            range(len(params.probability_array)), weights=params.probability_array
+        )[0]
+        match outcome:
+            case Outcome.WIDE:
+                extra_runs = self.rnd.choices(
+                    range(len(WIDE_EXTRA_RUNS)), weights=WIDE_EXTRA_RUNS, k=1
+                )[0]
+                return Ball(wide=True, extra_runs=extra_runs)
+            case Outcome.NOBALL:
+                extra_runs = self.rnd.choices(
+                    range(len(NB_EXTRA_RUNS)), weights=NB_EXTRA_RUNS, k=1
+                )[0]
+                batter_runs = self.rnd.choices(
+                    range(len(NB_BATTER_RUNS)), weights=NB_BATTER_RUNS, k=1
+                )[0]
+                return Ball(noball=True, extra_runs=extra_runs, batter_runs=batter_runs)
+            case _:
+                raise ValueError(f"Unexpected {outcome=}")
--- tests/model/test_balluator.py
+++ tests/model/test_balluator.py
@@ -6,6 +6,20 @@ import pytest
 from app.model.balluator import BallParams, Balluator
 from app.model.scorebook import Ball
 
+FAKE_PROBS = {
+    "p_wide": 0.03,
+    "p_noball": 0.04,
+    "p_bye": 0.05,
+    "p_legbye": 0.06,
+    "p_wicket": 0.07,
+    "p_dotball": 0.10,
+    "p_single": 0.11,
+    "p_two": 0.12,
+    "p_three": 0.13,
+    "p_four": 0.14,
+    "p_six": 0.15,
+}
+
 
 @dataclass
 class FakeRandom(Random):
@@ -23,30 +37,31 @@ def test_creation():
     assert Balluator(rng)
 
 
+# @pytest.mark.skip("")
 @pytest.mark.parametrize(
     "parms,rands,expected,desc",
     [
-        (BallParams(), [1.0], Ball(), "nothing happens"),
         (
-            BallParams(p_noball=0.05),
-            [
-                0.01,
-            ],
-            Ball(noball=True),
-            "nb",
+            BallParams(**FAKE_PROBS),
+            [0.01, 0.001],
+            Ball(wide=True, extra_runs=1),
+            "wide",
         ),
         (
-            BallParams(p_noball=0.05, p_wide=0.10),
-            [
-                0.07,
-            ],
-            Ball(wide=True),
-            "wide",
+            BallParams(**FAKE_PROBS),
+            [0.05, 0.001, 0.001],
+            Ball(noball=True, extra_runs=1),
+            "nb",
         ),
     ],
 )
-def test_result(parms, rands, expected, desc):
+def test_result(parms: BallParams, rands: list[float], expected: Ball, desc: str):
     rng = FakeRandom()
     rng.sequence = rands
     obj = Balluator(rng)
     assert obj.result(parms) == expected, desc
+
+
+def test_probs_combined_in_correct_order():
+    parms = BallParams(**FAKE_PROBS)
+    assert sorted(FAKE_PROBS.values()) == parms.probability_array
