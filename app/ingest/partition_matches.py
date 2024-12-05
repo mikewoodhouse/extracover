40264f2ebede3f74a5ece29b2c222443946ec792
Mike Woodhouse <mikewoodhouse@gmail.com>
2024-12-05 14:46:08 +0000

    feat: next time we import everything, identify & keep separate the 100 matches

--- app/ingest/partition_matches.py
+++ app/ingest/partition_matches.py
@@ -21,6 +21,9 @@ for p in Path("all").glob("*.json"):
     with p.open() as f:
         m = json.load(f)
     match_type = m["info"]["match_type"]
+    balls_per_over = m["info"]["match_type"]["balls_per_over"]
+    if balls_per_over == "5":
+        match_type = "TheHundred"
     gender = m["info"]["gender"]
     c[(match_type, gender)] += 1
     target_dir = Path("matches") / gender / match_type
