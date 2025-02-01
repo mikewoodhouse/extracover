17c480cd23f3fe680a8cbbdafd68f91c00d64677
MIke Woodhouse <mikewoodhouse@gmail.com>
2025-02-01 12:01:21 +0000

    test: mocking (is harder than it ought ot be)

--- tests/matchday/viewmodels/test_book_builder.py
+++ tests/matchday/viewmodels/test_book_builder.py
@@ -1,24 +1,32 @@
-from unittest.mock import Mock
+import sqlite3
 
-import pytest
+import pytest  # noqa: F401
 
+from matchday.data import BookRepository
 from matchday.models import Book
 from matchday.viewmodels.book_builder import BookBuilder
 
 
 @pytest.fixture
-def buidler():
-    repo = Mock()
+def builder(mocker):
+    repo = BookRepository(sqlite3.connect(":memory:"))
+    mocker.patch("matchday.data.BookRepository.add").return_value = 1
     bc = BookBuilder(book=Book(), repo=repo)
     bc.set_team_name("team_1", "Banana")
     bc.set_team_name("team_2", "Grapefruit")
     return bc
 
 
-def test_can_construct(buidler):
-    assert buidler
+def test_can_construct(builder: BookBuilder):
+    assert builder
 
 
-def test_setting_team_names(buidler):
-    assert buidler.book.team_1.name == "Banana"
-    assert buidler.book.team_2.name == "Grapefruit"
+def test_setting_team_names(builder: BookBuilder):
+    assert builder.book.team_1.name == "Banana"
+    assert builder.book.team_2.name == "Grapefruit"
+
+
+def test_saving_via_repo(builder: BookBuilder):
+    book_id = builder.add()
+    assert isinstance(book_id, int)
+    assert book_id >= 1
